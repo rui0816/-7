c30b252d3d05c86ff39fd38ba06e86ab9cf16b36
rui0816 <89883195+rui0816@users.noreply.github.com>
2021-09-22 10:07:37 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,10 @@ const app = Vue.createApp({
           // q2: 'かかか',
           // q3: 'ききき',
         }
+         stage4: {
+          q1: '',
+          // q2: 'かかか',
+          // q3: 'ききき',
       },
 
       /* それぞれの問題が正解かどうか
@@ -48,7 +52,11 @@ const app = Vue.createApp({
           false, // 3-1
           // false, // 3-2
           // false, // 3-3
-        ]
+        ],
+        stage4: [
+          false, // 3-1
+          // false, // 3-2
+          // false, // 3-3
       },
 
       /* ステージの問題が全て正解かどうか */
@@ -56,6 +64,7 @@ const app = Vue.createApp({
         stage1: false,
         stage2: false,
         stage3: false,
+        stage4: false,
       },
 
       /* 次のステージを表示するかどうか
@@ -64,6 +73,7 @@ const app = Vue.createApp({
       next: {
         stage1: false,
         stage2: false,
+        stage3: false,
       },
     }
   },
